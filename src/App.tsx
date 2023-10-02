04dce60144792253b733d1682f8e554da2f74441
Joaquin Beltran <contact@joaquinmx.com>
2023-10-02 00:15:00 -0600

    [WIP] Navigation and Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
 import {
-  IonApp,
-  IonContent,
-  IonHeader,
-  IonTitle,
-  IonToolbar,
+  IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs,
 } from '@ionic/react';
 import React from 'react';
-
+import { Redirect, Route } from 'react-router-dom';
+import { IonReactRouter } from '@ionic/react-router';
+import { home as homeIcon, settings as settingsIcon} from 'ionicons/icons';
+import HomePage from './pages/HomePage';
+import SettingsPage from './pages/SettingsPage';
+import EntryPage from './pages/EntryPage';
 const App: React.FC = () => {
   return (
     <IonApp>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>My App</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        Add some content here…
-      </IonContent>
+      <IonReactRouter>
+        <IonTabs>
+          <IonRouterOutlet>          
+            <Redirect exact path='/' to='/home'/>
+            <Route exact path='/home'>
+              <HomePage />
+            </Route>
+            <Route exact path='/entries/:id'>
+              <EntryPage />
+            </Route>
+            <Route exact path='/settings'>
+              <SettingsPage />
+            </Route>
+          </IonRouterOutlet>
+          <IonTabBar slot='bottom' >
+            <IonTabButton tab='home' href='/home'>
+              <IonIcon icon={homeIcon}/>
+              <IonLabel>Home</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab='settings' href='/settings'>
+              <IonIcon icon={settingsIcon}/>
+              <IonLabel>settings</IonLabel>
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+      </IonReactRouter>
     </IonApp>
   );
 };
---
+++ src/data.ts
@@ -0,0 +1,17 @@
+export const entries = [
+    {
+        id: '1',
+        title: 'first entry',
+        description: 'This is the first entry'
+    },
+    {
+        id: '2',
+        title: 'second entry',
+        description: 'This is the second entry'
+    },
+    {
+        id: '3',
+        title: 'third entry',
+        description: 'This is the third entry'
+    },
+]
